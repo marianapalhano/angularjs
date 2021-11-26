be99f7fe50ff86dfebbc0bf3bf93fd45e1981a57
Mariana <marianaufc@gmail.com>
2021-11-26 17:17:12 -0300

    creates name calculator app

--- p01-name-calculator/app.js
+++ p01-name-calculator/app.js
@@ -2,6 +2,20 @@
     'use strict';
     angular.module('NameCalculator', [])
         .controller('NameCalculatorController', function($scope) {
-            
+            $scope.name = '';
+            $scope.totalValue = 0;
+
+            $scope.displayNumeric = function() {
+                var totalNameValue = calculateNumeric($scope.name);
+                $scope.totalValue = totalNameValue;
+            }
+
+            function calculateNumeric(string) {
+                var totalStringValue = 0;
+                for (var i = 0; i < string.length; i++) {
+                    totalStringValue += string.charCodeAt(i);
+                }
+                return totalStringValue;
+            }
         })
 })();
