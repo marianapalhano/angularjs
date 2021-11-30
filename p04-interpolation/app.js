4a7d7018cee3fe0845260dc15fdc0a93e1bd6de3
Mariana <marianaufc@gmail.com>
2021-11-30 18:21:55 -0300

    adds interpolation project part 2

--- p04-interpolation/app.js
+++ p04-interpolation/app.js
@@ -3,13 +3,18 @@
     angular.module('MsgApp', [])
         .controller('MsgController', MsgController);
         
-        MsgController.$inject = ['$scope'];
+        MsgController.$inject = ['$scope'];       
 
         function MsgController ($scope) {
             $scope.name = "Mariana";
+            $scope.state = "hungry";
 
             $scope.sayMessage = function() {
                 return "Hello World!";
+            }
+
+            $scope.feedPuppy = function() {
+                $scope.state = "happy";
             }
         }
 })();
